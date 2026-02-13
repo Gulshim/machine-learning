efe66631d9f3e658073da5a0b73d81de1a1ce6a4
Gulshim Rashmi <148684780+Gulshim@users.noreply.github.com>
2026-02-13 10:57:44 +0530

    Add files via upload

---
+++ task4.py
@@ -0,0 +1,85 @@
+# a=97.456
+# print(abs(a))
+
+# a=23.52
+# print(round(a))
+
+# a=34.90
+# print(int(a))
+
+# a=899
+# print(complex(a))
+
+# a=90.09
+# print(divmod(a))
+
+# a="34.5"
+# print(a.rpartition("."))
+
+# a="9.4"
+# print("partition()")
+
+# a = "hello"
+# b = a.upper()
+# print(b)
+
+# a = 21324365.78843
+# b = str(a).lower()
+# print(b)
+
+# a = "HELLO"
+# b = a.title()
+# print(b)
+
+# a = 21324365.78843
+# b = str(a).title()
+# print(b)
+
+# |day-->6
+
+# ---->first true or false code --and gate --or gate
+# a=input("Enter your choice:") 
+# if a=="chocolate lover" or a=="biscuit lover":
+#    print("yes")                         
+# else:
+#    print("no")
+
+# a=int(input("Enter your value:"))
+# if a== 1:
+#    print("cat")
+# elif a == 2:
+#    print("dog")
+# elif a== 3:
+#    print("rabbit")
+# else:
+#    print("wrong choice")
+
+# nested Loop 
+
+
+
+
+
+
+# day-->7
+# for loop
+
+# for i in range(5):
+#   print(i)
+
+# num1=int(input("Enter your first number:"))
+# num2=int(input("Enter your second number:"))
+# num3=int(input("Enter your third number:"))
+# for i in range(1):
+#     if num1>num2:
+#         print("Greater number is:",num1)
+#     elif num2>num3:
+#         print("Greater number is:",num2)
+#     elif num3>num1:
+#         print("Greater number is:",num3)
+#     else:
+#         print("all are equal")
+
+
+for i in range(1,30,3):
+    print(i)
